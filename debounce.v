8b9eba42676cfb9d6a4333de3d46355eade8eaf3
LIU Dongyuan <liu.dongyuan@gmail.com>
2013-01-10 15:00:38 +0800

    debounce module.

---
+++ debounce.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+
+module debounce(
+    input wire clk,
+    input wire button,
+    output reg click
+);
+
+    reg [2:0] cnt;
+    wire clk_1ms;
+
+    localparam threshold = 10;
+
+    timer_1ms m0(clk, clk_1ms);
+
+    always @(posedge clk_1ms)
+    begin
+        if (button == 0) begin
+            click = 0;
+            cnt = 0;
+        end
+        else begin
+            if (cnt < threshold) begin
+                cnt = cnt + 1;
+                click = 0;
+            end
+            else begin
+                click = 1;
+            end
+        end
+    end
+
+endmodule
--- top.v
+++ top.v
@@ -4,10 +4,10 @@ module top(
     input wire clk, reset,
     input wire [1:0] btn1,
     input wire [1:0] btn2,
-    output wire hs, vs,
+    output wire hsync, vsync,
     output wire [2:0] rgb
 );
 
-    vga_sync vga_sync(.clk(clk), .hs(hs), .vs(vs));
+    vga_sync vga_sync(.clk(clk), .hsync(hsync), .vsync(vsync));
 
 endmodule
